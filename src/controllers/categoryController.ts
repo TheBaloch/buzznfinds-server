a35eb74e22090f76cce87bf79217a6d33b5ebd96
TheBaloch <abdullahbaloch797@gmail.com>
2024-08-06 17:52:39 +0500

    added limit to category to get blogs

--- src/controllers/categoryController.ts
+++ src/controllers/categoryController.ts
@@ -16,19 +16,29 @@ export const getCategory = async (req: Request, res: Response) => {
 
 export const getCategoryBySlug = async (req: Request, res: Response) => {
   const { slug } = req.params;
+  const { limit } = req.query;
 
   try {
     const categoryRepository = AppDataSource.getRepository(Category);
-    const category = await categoryRepository.findOne({
-      where: { slug: slug },
-      relations: ["blogs"],
-    });
+
+    const categoryQuery = categoryRepository
+      .createQueryBuilder("category")
+      .leftJoinAndSelect("category.blogs", "blog")
+      .where("category.slug = :slug", { slug });
+
+    if (limit) {
+      categoryQuery.limit(Number(limit));
+    }
+
+    const category = await categoryQuery.getOne();
+
     if (!category) {
       return res.status(404).json({ message: "Category not found" });
     }
+
     return res.status(200).json(category);
   } catch (error) {
-    console.error("Error fetching blog:", error);
+    console.error("Error fetching category:", error);
     return res.status(500).json({ message: "Internal server error", error });
   }
 };
