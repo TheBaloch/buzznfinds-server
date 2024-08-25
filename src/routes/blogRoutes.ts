b67250793ef0cfd4ea2376b8441b36bf22004cf6
TheBaloch <abdullahbaloch797@gmail.com>
2024-08-25 20:37:30 +0500

    final changes

--- src/routes/blogRoutes.ts
+++ src/routes/blogRoutes.ts
@@ -13,7 +13,7 @@ const router = Router();
 
 router.post("/blog/generate", generateBlog);
 // router.post("/blog", createBlog);
-router.get("/blog", getBlogs);
+//router.get("/blog", getBlogs);
 router.get("/blog/latest", getLatestBlogs);
 router.get("/blog/:slug", getBlogBySlug);
 // router.put("/blog/:id", updateBlog);
