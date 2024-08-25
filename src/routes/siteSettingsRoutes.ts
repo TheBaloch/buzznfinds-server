a4be7084cd83707cf59d7e5f38e4759dd20f224e
TheBaloch <abdullahbaloch797@gmail.com>
2024-08-25 20:44:02 +0500

    fine by me

--- src/routes/siteSettingsRoutes.ts
+++ src/routes/siteSettingsRoutes.ts
@@ -4,6 +4,6 @@ import { getSettings } from "../controllers/siteSettingsController";
 const router = Router();
 
 router.get("/settings", getSettings);
-router.put("/settings", getSettings);
+//router.put("/settings", getSettings);
 
 export default router;
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -13,8 +13,8 @@ import {
 const router = express.Router();
 
 // User routes
-router.post("/users/register", registerUser);
-router.post("/users/login", loginUser);
+//router.post("/users/register", registerUser);
+//router.post("/users/login", loginUser);
 
 router.get(
   "/users/me",
@@ -23,11 +23,11 @@ router.get(
 );
 
 // Email verification routes
-router.post("/users/verify-email", verifyEmail);
-router.post("/users/resend-verification-email", resendVerificationEmail);
+//router.post("/users/verify-email", verifyEmail);
+//router.post("/users/resend-verification-email", resendVerificationEmail);
 
 // Password reset routes
-router.post("/users/request-password-reset", requestPasswordReset);
-router.post("/users/reset-password", resetPassword);
+//router.post("/users/request-password-reset", requestPasswordReset);
+//router.post("/users/reset-password", resetPassword);
 
 export default router;
